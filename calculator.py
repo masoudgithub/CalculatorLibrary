9cd348a9630a7011d37c80f84c6f5112317fc85d
masoudgithub <masoudshiroie@gmail.com>
2019-06-07 11:44:44 +0200

    the .yml file is added

--- calculator.py
+++ calculator.py
@@ -1,8 +1,11 @@
 """
 calculator library containing basic math operatoins.
 """
-def add(firts_item, second_item):
-	return first_item + second_item
+
+
+def add(first_item, second_item):
+    return first_item + second_item
+
 
 def subtract(first_item, second_item):
-	return first_item - second_item
+    return first_item - second_item
